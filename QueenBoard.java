f06cd735e6fa5eab5cd3363d5255718ec9117ae1
vsofat <vsofat00@stuy.edu>
2019-02-12 18:45:23 -0500

    first commit

---
+++ QueenBoard.java
@@ -0,0 +1,27 @@
+public class QueenBoard {
+
+  private int sum;
+  private int[][] board;
+
+  public static void main(String[] args) {
+  }
+
+  public QueenBoard(int size) {
+  }
+
+  public String toString() {
+  }
+
+  public boolean solve() {
+  }
+
+  public int countSolutions() {
+  }
+
+  private boolean addQueen(int r, int c) {
+  }
+
+  private boolean removeQueen(int r, int c) {
+  }
+
+}
