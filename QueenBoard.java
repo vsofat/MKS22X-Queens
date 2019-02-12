b351aca1611ec7fc2dbd9556ddbe60cf0a23612f
vsofat <vsofat00@stuy.edu>
2019-02-12 18:58:32 -0500

    toString works with constructor; tested

--- QueenBoard.java
+++ QueenBoard.java
@@ -5,21 +5,39 @@ public class QueenBoard {
 
   //main for testing
   public static void main(String[] args) {
+    QueenBoard  board = new QueenBoard(8);
+    System.out.println(board.toString());
+
+    QueenBoard  board2 = new QueenBoard(5);
+    System.out.println(board2.toString());
   }
 
-  // constructors
+  // constructor
   public QueenBoard(int size) {
     board = new int[size][size];
-    for (int[] x: board) {
-      for (int y: x) {
-        y = 0;
+    for (int[] row: board) { // each row
+      for (int col: row) { // each column in row
+        col = 0; // set everything to 0
       }
     }
   }
 
+
   public String toString() {
+    String result = "";
+    for (int[] row : board) { //each row
+      for (int col : row) {
+        if (col == -1) { // value for queen
+          result += "Q ";} // Q is symbol for queen
+    else result += " 0 "; // if not a queen put a underscore to start with
+  }
+  result = result.substring(0, result.length() - 1);
+  result += "\n";
+}
+return result;
   }
 
+/*
   public boolean solve() {
   }
 
@@ -31,5 +49,7 @@ public class QueenBoard {
 
   private boolean removeQueen(int r, int c) {
   }
+
+  */
 
 }
